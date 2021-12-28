93641b64bafd96d58cf2ddf3ad641bebc65daa43
Rubin Barclay <rubinbarclay@protonmail.com>
2021-12-28 12:00:05 +0100

    added basic camera functionality

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from "expo-camera";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useRef, useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
-  return (
+  const [cameraAccess, setCameraAccess] = useState(false);
+  const [picture, setPicture] = useState(null);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    requestCameraAccess();
+  }, []);
+
+  const requestCameraAccess = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    setCameraAccess(status === "granted");
+  };
+
+  const takePicture = async () => {
+    const picture = await cameraRef.current.takePictureAsync();
+    setPicture(picture.uri);
+    console.log(picture);
+  };
+
+  return cameraAccess ? (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Camera style={styles.camera} ref={cameraRef}>
+        <TouchableOpacity style={styles.cameraBtn} onPress={takePicture} />
+      </Camera>
+      <StatusBar style="auto" />
+    </View>
+  ) : (
+    <View style={styles.container}>
+      <Text>This app requires your camera in order to work</Text>
+      <TouchableOpacity
+        style={styles.requestPermission}
+        onPress={requestCameraAccess}
+      >
+        <Text>Allow camera access</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +47,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    width: "100%",
+  },
+  cameraBtn: {
+    width: 80,
+    height: 80,
+    marginBottom: 20,
+    // backgroundColor: "#FFF",
+    borderRadius: 100,
+    borderWidth: 5,
+    borderColor: "#FFF",
+  },
+  requestPermission: {
+    backgroundColor: "#DDD",
+    borderRadius: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    margin: 10,
   },
 });
