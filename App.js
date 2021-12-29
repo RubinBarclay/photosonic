ce6fb1b6e8ef3f9d84faa5a67f53263f4707dee6
Rubin Barclay <rubinbarclay@protonmail.com>
2021-12-29 17:10:06 +0100

    added google clound vision API

--- App.js
+++ App.js
@@ -9,11 +9,14 @@ import {
   View,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import * as ImageManipulator from "expo-image-manipulator";
+import config from "./config.json";
 
 export default function App() {
   const [cameraAccess, setCameraAccess] = useState(false);
   const [showPreview, setShowPreview] = useState(false);
   const [picture, setPicture] = useState(null);
+  const [base64, setBase64] = useState(null);
   const cameraRef = useRef(null);
 
   useEffect(() => {
@@ -27,13 +30,59 @@ export default function App() {
 
   const takePicture = async () => {
     const picture = await cameraRef.current.takePictureAsync();
-    setPicture(picture);
+    const resizedPhoto = await ImageManipulator.manipulateAsync(
+      picture.uri,
+      [],
+      // [{ resize: { width: 300 } }], // resize to width of 300 and preserve aspect ratio
+      { base64: true, compress: 0.65, format: "jpeg" }
+    );
+    // console.log(Object.keys(resizedPhoto)); // { height, width, uri, base64}
+    // console.log(resizedPhoto);
+    setPicture(resizedPhoto);
+    setBase64(resizedPhoto.base64);
     setShowPreview(true);
   };
 
   const closePreview = () => {
     setPicture(null);
     setShowPreview(false);
+  };
+
+  const callGoogleCloudVision = async () => {
+    // const base64 = await FileSystem.readAsStringAsync(picture.uri, {
+    //   encoding: "base64",
+    // });
+    const response = await fetch(
+      config.googleCloud.api + config.googleCloud.apiKey,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          requests: [
+            {
+              image: {
+                content: base64,
+              },
+              features: [
+                { type: "LABEL_DETECTION", maxResults: 3 },
+                // { type: "LANDMARK_DETECTION", maxResults: 5 },
+                // { type: "FACE_DETECTION", maxResults: 5 },
+                // { type: "LOGO_DETECTION", maxResults: 5 },
+                // { type: "TEXT_DETECTION", maxResults: 5 },
+                // { type: "DOCUMENT_TEXT_DETECTION", maxResults: 5 },
+                // { type: "SAFE_SEARCH_DETECTION", maxResults: 5 },
+                // { type: "IMAGE_PROPERTIES", maxResults: 5 },
+                // { type: "CROP_HINTS", maxResults: 5 },
+                // { type: "WEB_DETECTION", maxResults: 5 },
+              ],
+            },
+          ],
+        }),
+      }
+    );
+
+    const data = await response.json();
+
+    console.log(data.responses);
   };
 
   return cameraAccess ? (
@@ -45,7 +94,10 @@ export default function App() {
               <Ionicons name="close" size={16} /> Retake
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.searchBtn}>
+          <TouchableOpacity
+            style={styles.searchBtn}
+            onPress={callGoogleCloudVision}
+          >
             <Text style={{ fontSize: 18 }}>
               Find song <Ionicons name="musical-notes" size={16} />
             </Text>
